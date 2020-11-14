ded1f1e6187f70b7947695e4d0702c26fc2ea074
biggsbenjamin <bb2515@ic.ac.uk>
2020-11-14 23:32:17 +0000

    halved latency, partitioned arrays

--- gemm.cc
+++ gemm.cc
@@ -11,18 +11,21 @@ void gemm(
   volatile int *a,
   volatile int *b,
   volatile int *c) {
-#pragma HLS INTERFACE m_axi port = a offset = slave bundle = a_port
-#pragma HLS INTERFACE m_axi port = b offset = slave bundle = b_port
-#pragma HLS INTERFACE m_axi port = c offset = slave bundle = c_port
+#pragma HLS INTERFACE m_axi port = a offset = slave bundle = a_port //axi4 are streaming protocols - can only go one way
+#pragma HLS INTERFACE m_axi port = b offset = slave bundle = b_port //read in
+#pragma HLS INTERFACE m_axi port = c offset = slave bundle = c_port //write out
 #pragma HLS INTERFACE s_axilite port = return bundle = CONTROL_BUS
 
-  int a_buff[M][N];
+  int a_buff[M][N]; //these are buffers to store the streamed info
+#pragma HLS array_partition variable=a_buff complete dim=2
   int b_buff[O][N];
+#pragma HLS array_partition variable=b_buff complete dim=2
   int c_buff[M][O];
+//#pragma HLS array_partition variable=c_buff complete dim=0 // this was a very dumb thing to do
 
   // Load A & B
-  memcpy(&a_buff[0][0], const_cast<int*>(a), sizeof(int) * M * N);
-  memcpy(&b_buff[0][0], const_cast<int*>(b), sizeof(int) * O * N);
+  memcpy(&a_buff[0][0], const_cast<int*>(a), sizeof(int) * M * N); //only works for a top level fn or arg
+  memcpy(&b_buff[0][0], const_cast<int*>(b), sizeof(int) * O * N); //burst access from memory - pipelined, for buffer storage
 
   // Matrix Multiplication
   for (int m = 0; m < M; m++) {

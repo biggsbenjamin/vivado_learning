8a61c73a91a4833c9459371684acd8355ac67e6b
biggsbenjamin <bb2515@ic.ac.uk>
2020-11-24 14:09:45 +0000

    reorganise2

--- add_ex.h
+++
@@ -1,8 +0,0 @@
-int add_ex(int in_a, int in_b);
-
-//image stream ex
-#include "hls_stream.h"
-
-typedef hls::stream<ap_axiu<16,1,1,1>>              AXI_STREAM;
-typedef hls::Mat<MAX_HEIGHT, MAX_WIDTH, HLS_8UC2>   YUV_IMAGE;
-typedef hls::Mat<MAX_HEIGHT, MAX_WIDTH, HLS_8UC3>   RGB_IMAGE;
